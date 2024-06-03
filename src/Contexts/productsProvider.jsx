a02dfc6fbcb2844986dfbf98b46ce3c61376aa3a
aminrostamii <90005278+aminrostamii@users.noreply.github.com>
2024-06-03 19:05:17 +0330

    Update productsProvider.jsx

--- src/Contexts/productsProvider.jsx
+++ src/Contexts/productsProvider.jsx
@@ -4,38 +4,38 @@ import api from "../Services/Api";
 const ProductsContext = createContext();
 
 const ProductsProvider = ({ children }) => {
-    
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        const fetchProducts = async () => {
-            try {
-                setProducts((await api.get('/products')));
-            } catch (error) {
-                console.log(error.message);
-            }
-        }
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await api.get('/products');
+        setProducts(response.data); // Ensure response data is set correctly
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
 
-        fetchProducts();
-    }, []);
+    fetchProducts();
+  }, []);
 
-    return (
-        <ProductsContext.Provider value={products}>
-            {children}
-        </ProductsContext.Provider>
-    );
-}
+  return (
+    <ProductsContext.Provider value={products}>
+      {children}
+    </ProductsContext.Provider>
+  );
+};
 
-const useProducts=()=>{
-   const products= useContext(ProductsContext)
-   return products;
-}
+const useProducts = () => {
+  const products = useContext(ProductsContext);
+  return products;
+};
 
-const useProductDetails=(id)=>{
-         const products=useContext(ProductsContext)
-         const result=products.find((product)=>product.id === id)
-         return result;
-}
+const useProductDetails = (id) => {
+  const products = useContext(ProductsContext);
+  const result = products.find((product) => product.id === id);
+  return result;
+};
 
 export default ProductsProvider;
-export {useProducts,useProductDetails} ;
+export { useProducts, useProductDetails };
